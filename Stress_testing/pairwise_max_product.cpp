e1a1e93b0601443348feb867e3e4e1a7cbaf02be
thebigby10 <abidurrahman22@iut-dhaka.edu>
2024-01-25 01:34:27 +0600

    Optimized code

--- Stress_testing/pairwise_max_product.cpp
+++ Stress_testing/pairwise_max_product.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    unsigned long long int n; cin>>n;
-    vector<unsigned long long int >a(n,0);
-    for(unsigned long long int i=0;i<n;i++) cin>>a[i];
+    int n; cin>>n;
+    vector<long long int >a(n,0);
+    for(int i=0;i<n;i++) cin>>a[i];
     sort(a.begin(),a.end());
     cout<<a[a.size()-1]*a[a.size()-2]<<endl;
     return 0;
--- Stress_testing/stress_test.py
+++ Stress_testing/stress_test.py
@@ -8,39 +8,54 @@ import time
 import threading
 
 file_name = "pairwise_max_product.cpp"
+time_limit = 0.5
 test_case = 1000
+
 
 n = -1
 a = []
 
 def output_solution():
     #generating output 1
-    os.system(f"g++ {file_name} -o solution && ./solution < input.txt > solution_output.txt")
+    os.system(f"g++ {file_name} -o solution")
 
 def output_bruteforce():
     #generating bruteforce output
-    os.system("g++ brute_force.cpp -o bruteforce && ./bruteforce < input.txt > bruteforce_output.txt")
+    os.system("g++ brute_force.cpp -o bruteforce")
+
+t1 = threading.Thread(target = output_solution,args = ());
+t2 = threading.Thread(target = output_bruteforce,args = ());
+t1.start()
+t2.start()
+t1.join()
+t2.join()
 
 for _ in range(test_case):
     n = -1
     a = []
     with open("input.txt","w") as input_file:
-        n = random.randint(2,10000)
+        n = random.randint(2,1000)
         input_file.write(f"{n}\n")
         for i in range(n): 
             a.append(random.randint(0,1000000000))
             input_file.write(f"{a[-1]} ")
         input_file.write("\n")
     print(n,a)
-    # generating output
-    t1 = threading.Thread(target = output_solution,args = ());
-    t2 = threading.Thread(target = output_bruteforce,args = ());
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-    #generating output
-
+    
+    # start_time = time.time()
+    os.system("./solution < input.txt > solution_output.txt")
+    # end_time = time.time()
+    # if(end_time - start_time > time_limit):
+    #     print("TLE")
+    #     break
+    
+    # start_time = time.time()
+    os.system("./bruteforce < input.txt > bruteforce_output.txt")
+    # end_time = time.time()
+    # if(end_time - start_time > time_limit):
+    #     print("TLE")
+    #     break
+    
     if(os.system("diff solution_output.txt bruteforce_output.txt")==0):
         print("OK")
     else:
